772c9d068f9048550deac49b4b550fc2ef3a05c8
shomil03 <shomil02020@gmail.com>
2024-01-22 01:39:45 +0530

    Initial Commit

---
+++ Barcode-Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode-Scanner
+//
+//  Created by Shomil Singh on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
